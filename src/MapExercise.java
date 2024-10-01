e4e8cb03e42760c3189654741bbfc5538ffbc24b
Tumanov <tslavich@bk.ru>
2024-10-01 11:50:32 +0300

    Initial commit

---
+++ src/MapExercise.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapExercise {
+    public static void main(String[] args) {
+        Map<String, Double> map = new TreeMap<>();
+        System.out.println("Hello world!");
+    }
+}

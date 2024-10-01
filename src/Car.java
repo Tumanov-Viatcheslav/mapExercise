847cf2438d328792f538a187021e6fbdc8f4681b
Tumanov <tslavich@bk.ru>
2024-10-01 11:51:48 +0300

    Init

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    String model;
+    int year;
+    String color;
+
+    public Car() {
+    }
+
+    public Car(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
--- src/MapExercise.java
+++ src/MapExercise.java
@@ -1,10 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class MapExercise {
     public static void main(String[] args) {
-        Map<String, Double> map = new TreeMap<>();
-        System.out.println("Hello world!");
+        Map<String, Car> carMap = new HashMap<>();
+
+        carMap.put("Anton", new Car("BMW", 2020, "black"));
+        carMap.put("Boris", new Car("Tesla", 2022, "white"));
+        carMap.put("Yulia", new Car("Fiat", 2020, "red"));
+        carMap.put("Stas", new Car("Lada", 2000, "red"));
+
+        System.out.println(carMap);
+
+        String x1 = "Boris";
+        Car result1 = carMap.get(x1);
+        System.out.println(result1);
+
+        String x2 = "x";
+        Car result2 = carMap.get(x2);
+        System.out.println(result2);
     }
 }
